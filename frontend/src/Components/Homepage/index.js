aba91fb282cde2d696029535de6d5b5ff9cab4e3
hieuvo2307 <84066626+hieuvo2307@users.noreply.github.com>
2021-11-15 16:08:50 +0700

    Update index.js

--- frontend/src/Components/Homepage/index.js
+++ frontend/src/Components/Homepage/index.js
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h2>Home</h2>;
+import Header from '../Utils/Header'
+import Footer from '../Utils/Footer'
+
+export default function Homepage () {  
+  return (<>
+    <h1>This is a header</h1>
+    <Header />
+    <TestHeader />
+    <TestPagination itemsPerPage={9}/> 
+    <Footer />
+  </>)
 }
