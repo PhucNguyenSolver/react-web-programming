799176d6afbda43dbeed2cdb5badd4f89a758a28
Hieuvo2307 <hiev.vo@gmail.com>
2021-11-22 19:38:17 +0700

    Thêm chức năng Quản lý thành viên của QL

--- frontend/src/Components/ChangeInfoAccount/EditAccount.js
+++ frontend/src/Components/ChangeInfoAccount/EditAccount.js
@@ -1,92 +1,31 @@
 import React from 'react';
-const pic = "https://mona.media/wp-content/uploads/2021/10/guest-post-377x247.png";
+import { Button, Modal, Form, Col } from "react-bootstrap";
+import { useState } from "react";
+import ChangeForm from "./form.js";
 
 export default function EditAccount () {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
-    <button type="button" className="btn btn-outline-primary btn-sm" style={{float: 'right'}} data-bs-toggle="modal" data-bs-target="#exampleModal">
-      Chỉnh sửa
-    </button>
+      <Button variant="outline-primary" size="sm" onClick={handleShow} style={{float: 'right'}}>
+        Chỉnh sửa
+      </Button>
 
-     <div className="modal" id="staticSecond" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div className="modal-dialog modal-lg">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="staticBackdropLabel">Thông tin người dùng</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              <div className="row justify-content-md-center" style={{marginBottom: '2%'}}>
-                <div className="col-sm-6">
-                  <img className="img-fluid" style={{width: '100%'}} src={pic} alt=""/>
-                </div>
-              </div>
-              <div className="row justify-content-md-center">
-                <div  className="col-sm-10">
-                  <div className="mb-3 row">
-                    <label for="staticId" className="col-sm-4 col-form-label" style={{fontWeight: 'bold'}}>Id</label>
-                    <div className="col-sm-8">
-                      <input type="text" readonly className="form-control" style={{backgroundColor: 'white'}} id="staticId" value="2"/>
-                    </div>
-                  </div>
-                              
-                  <div className="mb-3 row">
-                    <label for="staticName" className="col-sm-4 col-form-label" style={{fontWeight: 'bold'}}>Tên người dùng</label>
-                    <div className="col-sm-8">
-                      <input type="text" readonly className="form-control" style={{backgroundColor: 'white'}} id="staticName" value="Nguyễn Hữu Bảo"/>
-                    </div>
-                  </div>
-                              
-                  <div className="mb-3 row">
-                    <label for="staticAccount" className="col-sm-4 col-form-label" style={{fontWeight: 'bold'}}>Tên tài khoản</label>
-                    <div className="col-sm-8">
-                      <input type="text" className="form-control" id="staticAccount" value="HuuBao"/>
-                    </div>
-                  </div>
-                              
-                  <div className="mb-3 row">
-                    <label for="staticPassword" className="col-sm-4 col-form-label" style={{fontWeight: 'bold'}}>Mật khẩu</label>
-                    <div className="col-sm-8">
-                      <input type="password" className="form-control" id="staticPassword" value="123456789"/>
-                    </div>
-                  </div>
-                              
-                  <div className="mb-3 row">
-                    <label for="staticEmail" className="col-sm-4 col-form-label" style={{fontWeight: 'bold'}}>Email</label>
-                    <div className="col-sm-8">
-                      <input type="text" className="form-control" id="staticEmail" value="bao.nguyen@gmail.com"/>
-                    </div>
-                  </div>
-                              
-                  <div className="mb-3 row">
-                    <label for="staticPhone" className="col-sm-4 col-form-label" style={{fontWeight: 'bold'}}>Số điện thoại</label>
-                    <div className="col-sm-8">
-                      <input type="text" className="form-control" id="staticPhone" value="0123459854"/>
-                    </div>
-                  </div>
-                              
-                  <div className="mb-3 row">
-                    <label for="staticAddress" className="col-sm-4 col-form-label" style={{fontWeight: 'bold'}}>Địa chỉ</label>
-                    <div className="col-sm-8">
-                      <input type="text" className="form-control" id="staticAddress" value="Tp.HCM"/>
-                    </div>
-                  </div>
-                              
-                  <div className="mb-3 row">
-                    <label for="staticRule" className="col-sm-4 col-form-label" style={{fontWeight: 'bold'}}>Vai trò</label>
-                    <div className="col-sm-8">
-                      <input type="text" readonly className="form-control" style={{backgroundColor: 'white'}} id="staticRule" value="Quản lý"/>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="modal-footer">       
-              <button type="button" className="btn btn-primary">Lưu thay đổi</button>
-            </div>
-          </div>
-        </div>
-      </div> 
+      <Modal size="lg" show={show} onHide={() => setShow(false)} aria-labelledby="example-modal-sizes-title-lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Thông tin người dùng</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ChangeForm/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Lưu thay đổi
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
---
+++ frontend/src/Components/ChangeInfoAccount/form.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Form, Col, Row } from "react-bootstrap";
+
+const infoForm = {
+  img: "https://product.hstatic.net/1000026716/product/028vn_710aac8009614321ba3103b049a0a3c4_large.png",
+  id: 1,
+  fullname: "Vo Thanh Hieu",
+  name: "HieuVo",
+  pass: "123456789",
+  email: "hiev.vo@gmail.com",
+  phone: "0123452341",
+  address: "Tien Giang",
+  rule: "Khach hang",
+  discount: true
+};
+
+export default function ChangeForm () {
+  const item = infoForm;
+  return <>
+
+    <div className="row justify-content-md-center" style={{marginBottom: '2%'}}>
+      <div className="col-sm-6">
+        <img className="img-fluid" style={{width: '100%'}} src={item.img} alt=""/>
+      </div>
+    </div>        
+    <div className="row justify-content-md-center">
+      <div  className="col-sm-10">
+        <Form>
+          <Form.Group className="mb-3" controlId="formId">
+            <Row>
+              <Form.Label column sm="4">
+                <h6>Id</h6>
+              </Form.Label>
+              <Col sm="8">
+              <Form.Control type="text" defaultValue={item.id} readOnly style={{backgroundColor: 'white'}}/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Form.Label column sm="4">
+                <h6>Tên người dùng</h6>
+              </Form.Label>
+              <Col sm="8">
+              <Form.Control type="text" defaultValue={item.fullname} readOnly style={{backgroundColor: 'white'}}/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Form.Label column sm="4">
+                <h6>Tên tài khoản</h6>
+              </Form.Label>
+              <Col sm="8">
+              <Form.Control type="text" defaultValue={item.name} readOnly style={{backgroundColor: 'white'}}/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Form.Label column sm="4">
+                <h6>Mật khẩu</h6>
+              </Form.Label>
+              <Col sm="8">
+              <Form.Control type="password" defaultValue={item.phone}/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Form.Label column sm="4">
+                <h6>Email</h6>
+              </Form.Label>
+              <Col sm="8">
+              <Form.Control type="text" defaultValue={item.email}/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Form.Label column sm="4">
+                <h6>Số điện thoại</h6>
+              </Form.Label>
+              <Col sm="8">
+              <Form.Control type="text" defaultValue={item.phone}/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Form.Label column sm="4">
+                <h6>Địa chỉ</h6>
+              </Form.Label>
+              <Col sm="8">
+              <Form.Control type="text" defaultValue={item.address}/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Form.Label column sm="4">
+                <h6>Vai trò</h6>
+              </Form.Label>
+              <Col sm="8">
+              <Form.Control type="text" defaultValue={item.rule} readOnly style={{backgroundColor: 'white'}}/>
+              </Col>
+            </Row>
+          </Form.Group>
+        </Form>
+      </div>
+    </div>
+  </>
+}
