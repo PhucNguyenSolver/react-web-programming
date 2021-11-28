5b115fafc9724d69b4353e839c61dc181db5f634
gianganlam0 <vios.tee972@gmail.com>
2021-11-28 15:44:43 +0700

    a

--- backend/Controller/ProductController.php
+++ backend/Controller/ProductController.php
@@ -7,13 +7,13 @@
     //if request is get, have one parameter name rq=deal
     if($_SERVER['REQUEST_METHOD'] == 'GET'){
         if(isset($_GET['rq'])){
-            if ($_GET['rq'] == 'deal'){
+            if ($_GET['rq'] == 'deal'){//?rq=deal
                 echo $productModel->getDealHot();
             }
             else if($_GET['rq'] == 'rate'){
                 echo $productModel->getTopRating();
             }
-            else if($_GET['rq'] == 'manu' && isset($_GET['name'])){
+            else if($_GET['rq'] == 'manu' && isset($_GET['name'])){//?rq=manu&name=dell
                 echo $productModel->getAllByManu($_GET['name']);
             }
             //else echo 404 not found
@@ -22,7 +22,7 @@
                 echo '404 not found';
             } 
         }
-        else if(isset($_GET['orderby'])){
+        else if(isset($_GET['orderby'])){//?orderby=newCost&a=0$b=15&asc=desc
             if(isset($_GET['a']) && isset($_GET['b']) && isset($_GET['asc'])){
                 echo $productModel->getTopBy($_GET['orderby'], $_GET['a'], $_GET['b'], $_GET['asc']);
             }
@@ -31,7 +31,7 @@
                 echo '404 not found';
             }
         }
-        else if(isset($_GET['id'])){
+        else if(isset($_GET['id'])){//?id=1
             echo $productModel->getProductByID($_GET['id']);
         }
         else{//404 not found
