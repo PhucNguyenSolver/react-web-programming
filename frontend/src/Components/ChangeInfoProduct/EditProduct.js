05dd9c03d829dd9e2cd9375cf64e183c286eb8c8
gianganlam0 <vios.tee972@gmail.com>
2021-11-29 08:23:17 +0700

    a

--- frontend/src/Components/ChangeInfoProduct/EditProduct.js
+++ frontend/src/Components/ChangeInfoProduct/EditProduct.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Modal } from "react-bootstrap";
 import { useState } from "react";
-import ChangeForm from "./form";
+import ChangeForm from "./EditForm";
 
 export default function EditProduct () {
   const [show, setShow] = useState(false);
